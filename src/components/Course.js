ec6e5f8590f534a0db9659f2c0cd1c3e0ce5fa27
Diego Pamo <diego.pamo@tecsup.edu.pe>
2021-04-14 16:02:29 -0500

    Punto 2

--- src/components/Course.js
+++ src/components/Course.js
@@ -2,10 +2,13 @@ import Header from './Header';
 import Content from './Content';
 
 const course = (props) => {
+    const exercis = props.course.parts
+    const suma = exercis.reduce((cont, {exercises}) => cont+exercises,0)
 return (
     <div>
         <Header header={props.course.name}/>
         <Content content={props.course.parts}/>
+        <p><b>Suma de los Exercises: {suma}</b></p>
     </div>
     );
 }
